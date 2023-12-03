c4129c9746b5eebcd85d444ebf1837ef5a894586
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2023-12-03 22:01:49 +0300

    up

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -10,7 +10,7 @@ function Summary() {
   const [awayData, setAwayData] = useState([]);
 
   useEffect(() => {
-    fetch('/summaryOverall')
+    fetch('https://ws-foot-stat.onrender.com/summaryOverall')
       .then(response => {
         if (!response.ok) {
           throw new Error(`Error: ${response.status} - ${response.statusText}`);
@@ -25,7 +25,7 @@ function Summary() {
   }, []);
 
   useEffect(() => {
-    fetch('/summaryHome')
+    fetch('https://ws-foot-stat.onrender.com/summaryHome')
       .then(response => {
         if (!response.ok) {
           throw new Error(`Error: ${response.status} - ${response.statusText}`);
@@ -40,7 +40,7 @@ function Summary() {
   }, []);
 
   useEffect(() => {
-    fetch('/summaryAway')
+    fetch('https://ws-foot-stat.onrender.com/summaryAway')
       .then(response => {
         if (!response.ok) {
           throw new Error(`Error: ${response.status} - ${response.statusText}`);
